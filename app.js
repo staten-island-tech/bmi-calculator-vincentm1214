f3b71ba0b8dc7b682c60db976891963b8633e4f3
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2023-10-03 10:11:57 -0400

    e

---
+++ app.js
@@ -0,0 +1,13 @@
+function divide(a){
+    console.log( x / y);
+}
+const x = Number(prompt("enter your weight in pounds"));
+const y = Number(prompt("enter your height in inches"));
+   divide(x, y);
+   test(;)
+   function multiply(b){
+    console.log( z * a);
+}
+const z = 703
+   multiply(z, a);
+test();
