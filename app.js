b74edb31d88e9e283f7c7b17d6fabab84216d35c
vincentm1214 <144461068+vincentm1214@users.noreply.github.com>
2023-10-05 10:15:45 -0400

    cries

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-// function getBMI(){
-//     return 
-// }
-
-
-
-
-
-
-
-function multiply(y , y){
-    console.log( y * y);
-    multiply(y * y)
+function getBMI(){
+    return weight / this.height
+    const weight = 
+    x = Number(prompt("enter your weight in kilograms"));
+    const height = 
+    y = Number(prompt("enter your height in centimeters"));
 }
+function multiply(y,y)
+    console.log(y*y)
+function checkBMI
+function init() {
+    let BMI = getBMI(x, y);
+    checkBMI(BMI);
+}
+
+
+
+
 
 function divide(x , y){
     console.log( x / y);
 }
-const x = Number(prompt("enter your weight in kilograms"));
-const y = Number(prompt("enter your height in centimeters"));
+
+
    divide(x, y);
   
 test();
